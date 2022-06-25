bf2a54d54641b7ec5556596a9714fa23eabd9d18
Innovative Studios <innovativestudios@outlook.com>
2022-06-24 21:51:37 -0500

    Update 06242022@2150

--- components/content/Card.vue
+++ components/content/Card.vue
@@ -1,9 +1,9 @@
 <template>
-	<section class="border-r border-b shadow-sm bg-gray-800 !h-32 p-3">
-		<article class="grid grid-flow-row grid-cols-1 lg:grid-cols-3 lg:gap-3 items-center w-full h-full">
+	<section class="border-r border-b shadow-sm bg-gray-800 !h-40 p-3">
+		<article class="grid grid-flow-row grid-cols-1 lg:grid-cols-3 gap-3 items-center w-full h-full">
 			<component is="icon" class="col-span-1 text-center text-white" :class="icon" />
 			<div class="col-span-1 lg:col-span-2">
-				<p class="text-center text-white !text-xs sm:!text-sm lg:text-start">
+				<p class="text-center text-white !text-xs sm:!text-sm md:!text-base lg:text-start">
 					<NuxtLink class="!text-sm sm:!text-base md:!text-xl lg:!text-2xl font-semibold !pl-0" :to="link">{{ title }}</NuxtLink>
 					<br>
 					{{ description }}
