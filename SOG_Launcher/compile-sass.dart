9940c7f2e9b4c725a20a99e8552dcc0bf2f464f9
Innovative Studios <innovativestudios@outlook.com>
2022-08-19 22:34:16 -0500

    Removed SOG_Launcher

--- SOG_Launcher/compile-sass.dart
+++
@@ -1,7 +0,0 @@
-import 'dart:io';
-import 'package:sass/sass.dart';
-
-void main(List<String> arguments) {
-	var result = sass.compile(arguments[0]);
-	new File(arguments[1]).writeAsStringSync(result);
-}
