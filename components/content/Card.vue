82ad43b31b2a2a7f40a6d5c5585a8b7c13ed7943
InnovativeStudios <innovativestudios@outlook.com>
2022-09-03 02:56:55 -0500

    Update 09032022@0300

--- components/content/Card.vue
+++ components/content/Card.vue
@@ -8,11 +8,11 @@ defineProps<{
 </script>
 
 <template>
-	<section class="border-r border-b shadow-sm bg-gray-800 !h-40 p-3">
+	<section class="border-r border-b shadow-sm bg-zinc-900 !h-40 p-3">
 		<article class="grid grid-flow-row grid-cols-1 lg:grid-cols-3 gap-3 items-center w-full h-full">
 			<component is="icon" class="col-span-1 text-center text-white" :class="icon" />
 			<div class="col-span-1 lg:col-span-2">
-				<p class="text-center text-white !text-xs sm:!text-sm md:!text-base lg:text-start">
+				<p class="text-center text-gray-400 !text-xs sm:!text-sm md:!text-base lg:text-start">
 					<NuxtLink class="!text-sm sm:!text-base md:!text-xl lg:!text-2xl font-semibold !pl-0" :to="link">{{ title }}</NuxtLink>
 					<br>
 					{{ description }}
